9a6eee79126b8809e645a49e4e3b28c25dfec7e7
Tolik_mac <ttserunyan@inbox.ru>
2026-02-26 03:01:19 +0400

    added serializers

--- tasks/views.py
+++ tasks/views.py
@@ -5,4 +5,7 @@ from .serializers import TaskSerializer
 
 
 class TaskListAPIView(APIView):
-    def get(self, request)
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
