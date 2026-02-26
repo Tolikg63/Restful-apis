f51055485da05dc59c4c11a1647bf4771625768b
Tolik_mac <ttserunyan@inbox.ru>
2026-02-26 14:24:23 +0400

    added post method

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,4 +4,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', '']
+        fields = ['id', 'title', 'description', 'completed', 'created_at']
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework import status
 
 
 class TaskListAPIView(APIView):
@@ -9,3 +10,10 @@ class TaskListAPIView(APIView):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
+    
+    def post(selq, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
