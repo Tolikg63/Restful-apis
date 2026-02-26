c7136aa2e84a3e5e4266f7943dc15ec491fd902c
Tolik_mac <ttserunyan@inbox.ru>
2026-02-27 00:24:47 +0400

    added viewset

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import TaskListAPIView
+from django.urls import path, include
+from .views import TaskListAPIView, TaskDetailAPIView, TaskViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('tasks/', TaskListAPIView.as_view(), name='task-list')
+    # path('tasks/', TaskListAPIView.as_view(), name='task-list'),
+    # path('tasks/<int:pk>/', TaskDetailAPIView.as_view(), name='task-detail')
+    path('', include(router.urls))
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import get_object_or_404
 
 
 class TaskListAPIView(APIView):
@@ -17,3 +19,31 @@ class TaskListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDetailAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Task, pk=pk)
+    
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
